d545d2e79a173f8714a028c02bf8d4dbc5ca7257
Eric Uen <ikpassat@gmail.com>
2024-01-01 07:26:26 +0800

    110_Filtering Jobs: Tailwind Form Plugin & Text Inputs

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,3 @@
+<input type="text" name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}"
+    value="{{ $value }}"
+    class="w-full rounded-md py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,6 +1,24 @@
 <x-layout>
     <x-bread-crumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
 
+    <x-card class="mb-4 text-sm">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+            <div>
+                <div class="mb-1 font-semibold">Search</div>
+                <x-text-input name="name" value="" placeholder="Search for any text" />
+            </div>
+            <div>
+                <div class="mb-1 font-semibold">Salary</div>
+                <div class="flex space-x-1">
+                    <x-text-input name="min_salary" value="" placeholder="From" />
+                    <x-text-input name="max_salary" value="" placeholder="To" />
+                </div>
+            </div>
+            <div>3</div>
+            <div>4</div>
+        </div>
+    </x-card>
+
     @foreach ($jobs as $job)
         <x-job-card class="mb-4" :$job>
             <div>
