0c2a0c71b19933a3e92b0d62c5582d417413805d
Eric Uen <ikpassat@gmail.com>
2024-01-13 08:26:55 +0800

    120_Refactor Time: Refactoring Filtering Backend Logic

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +17,21 @@ class Job extends Model
         'IT', 'Finance', 'Sales', 'Marketing',
     ];
 
+    public function scopeFilter(Builder | QueryBuilder $query, array $filters): Builder | QueryBuilder
+    {
+        return $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('description', 'like', '%' . $search . '%');
+            });
+        })->when($filters['min_salary'] ?? null, function ($query, $min_salary) {
+            $query->where('salary', '>=', $min_salary);
+        })->when($filters['max_salary'] ?? null, function ($query, $max_salary) {
+            $query->where('salary', '<=', $max_salary);
+        })->when($filters['experience'] ?? null, function ($query, $experience) {
+            $query->where('experience', $experience);
+        })->when($filters['category'] ?? null, function ($query, $category) {
+            $query->where('category', $category);
+        });
+    }
 }
