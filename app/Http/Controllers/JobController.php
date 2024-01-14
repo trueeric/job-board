aa718a43e198816683664c400518d2e38c70e3f8
Eric Uen <ikpassat@gmail.com>
2024-01-14 17:48:03 +0800

    123_Employer: Searching By Employer Name

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -14,7 +14,7 @@ class JobController extends Controller
     {
         $filters = request()->only('search', 'min_salary', 'max_salary', 'experience', 'category');
 
-        return view('job.index', ['jobs' => Job::filter($filters)->get()]);
+        return view('job.index', ['jobs' => Job::with('employer')->filter($filters)->get()]);
     }
 
     /**
@@ -38,7 +38,8 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-        return view('job.show', compact('job'));
+        // return view('job.show', compact('job'));
+        return view('job.show', ['job' => $job->load('employer')]);
     }
 
     /**
