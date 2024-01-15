d7776454d0d287288c054b058f2faa9631ca380f
Eric Uen <ikpassat@gmail.com>
2024-01-15 10:43:52 +0800

    129_Applying for Jobs: Model, Factory, Relations & Migration

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
 {
@@ -21,6 +22,10 @@ class Job extends Model
     public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
+    }
+    public function jobApplications(): HasMany
+    {
+        return $this->hasMany(JobApplication::class);
     }
 
     public function scopeFilter(Builder | QueryBuilder $query, array $filters): Builder | QueryBuilder
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,11 +36,17 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-        // \App\Models\User::factory(10)->create();
+        foreach ($users as $user) {
+            $jobs = \App\Models\Job::inRandomOrder()->take(rand(0, 4))->get();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            foreach ($jobs as $job) {
+                \App\Models\JobApplication::factory()->create([
+                    'job_id'  => $job->id,
+                    'user_id' => $user->id,
+                ]);
+            }
+
+        }
+
     }
 }
