63c89982181c3420d9fed99eabc9ce99ea688cfc
Eric Uen <ikpassat@gmail.com>
2024-01-17 23:56:35 +0800

    136_My Applications: Cancelling Applications

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobApplication;
+
 class MyJobApplicationController extends Controller
 {
 
@@ -19,8 +21,13 @@ class MyJobApplicationController extends Controller
             ]);
     }
 
-    public function destroy(string $id)
+    public function destroy(JobApplication $myJobApplication)
     {
-        //
+        $myJobApplication->delete();
+
+        return redirect()->back()->with(
+            'success',
+            'Job application removed.'
+        );
     }
 }
--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -7,7 +7,7 @@
                 <div>
                     <div>Applied {{ $application->created_at->diffForHumans() }}</div>
                     <div>
-                        other{{ Str::plural('applicant', $application->job->job_applications_count - 1) }}
+                        Other{{ Str::plural(' applicant', $application->job->job_applications_count - 1) }}
                         {{ $application->job->job_applications_count - 1 }}
                     </div>
                     <div>
@@ -18,9 +18,23 @@
                         ${{ number_format($application->job->job_applications_avg_expected_salary) }}
                     </div>
                 </div>
-                <div>right</div>
+                <div>
+                    <form action="{{ route('my-job-applications.destroy', $application) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <x-button>Cancel</x-button>
+                    </form>
+                </div>
             </div>
         </x-job-card>
     @empty
+        <div class="rounded-md border border-dashed border-slate-300 p-8">
+            <div class="text-center font-medium ">
+                No job application yet.
+            </div>
+            <div class="text-center">
+                Go to find some jobs <a class="text-indigo-500 hover:underline" href="{{ route('jobs.index') }}">here</a>
+            </div>
+        </div>
     @endforelse
 </x-layout>
