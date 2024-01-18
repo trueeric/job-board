6d12c1eb1560f85ad4df26572aad816884e2519b
Eric Uen <ikpassat@gmail.com>
2024-01-19 03:29:33 +0800

    144_Employer: Adding Jobs Form

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -1,12 +1,14 @@
 <div>
-    <label for="{{ $name }}" class="mb-1 flex items-center">
-        <input type="radio" name="{{ $name }}" value="" @checked(!request('$name')) />
-        <span class="ml-2">All</span>
-    </label>
+    @if ($allOption)
+        <label for="{{ $name }}" class="mb-1 flex items-center">
+            <input type="radio" name="{{ $name }}" value="" @checked(!request('$name')) />
+            <span class="ml-2">All</span>
+        </label>
+    @endif
 
     @foreach ($optionsWithLabels as $label => $option)
         <label for="{{ $name }}" class="mb-1 flex items-center">
-            <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === request('$name')) />
+            <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === ($value ?? request('$name'))) />
             <span class="ml-2">{{ $label }}</span>
         </label>
     @endforeach
