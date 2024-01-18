5ab9494618c4367ee46770d913ae4b832de67783
Eric Uen <ikpassat@gmail.com>
2024-01-18 11:57:54 +0800

    142_Employer: Register as an Employer

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -13,6 +13,9 @@ class Employer extends Model
 {
     use HasFactory;
 
+    // protected $fillable = ['company_name'];
+    protected $fillable = ['company_name'];
+
     public function jobs(): HasMany
     {
         return $this->hasMany(Job::class);
