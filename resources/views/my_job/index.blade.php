7eb79b9817a078f6f2e99d54e2a73e5f16607479
Eric Uen <ikpassat@gmail.com>
2024-01-19 10:08:11 +0800

    149_Employer: Soft Deletes

--- resources/views/my_job/index.blade.php
+++ resources/views/my_job/index.blade.php
@@ -25,6 +25,11 @@
 
                 <div class="flex space-x-2">
                     <x-link-button href="{{ route('my-jobs.edit', $job) }}">Edit</x-link-button>
+                    <form action="{{ route('my-jobs.destroy', $job) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <x-button>Delete</x-button>
+                    </form>
                 </div>
 
             </div>
